0d41ceb9c22f757e8e3472bc891dc519ca30668f
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-21 21:46:40 +0300

    Modified CORS options to add Methods

--- src/configs/config.js
+++ src/configs/config.js
@@ -14,7 +14,8 @@ module.exports = {
     },
     COLS_USER: 'id first_name last_name email username level',
     CORS_OPTIONS: {
-        origin: '*'
+        origin: '*',
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
     },
     ALLOWED_LEVELS: [0,1,2],
     SALT_ROUNDS: 12,
